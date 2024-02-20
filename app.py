3efa2afd39c7e72e65d5f361f424f59cb89626eb
mrfakename <mrfakename@users.noreply.huggingface.co>
2024-02-20 20:54:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import gradio as gr
 from musiclib import musicgen
+from io import BytesIO
+import midi_util
+from midi_util import VocabConfig
 
 def gen(piano_only):
+    midi = ''
     for item in musicgen(piano_only=piano_only):
-        yield item
+        midi = item
+        yield item, None
+    bio = BytesIO()
+    cfg = VocabConfig.from_json('./vocab_config.json')
+    text = midi.strip()
+    mid = midi_util.convert_str_to_midi(cfg, text)
+    mid.save(bio)
+    return midi, bio.getvalue()
 with gr.Blocks() as demo:
     piano_only = gr.Checkbox(label="Piano Only")
     synth = gr.Button("Synthesize")
-    fileout = gr.Textbox(interactive=False)
-    synth.click(gen, inputs=[piano_only], outputs=[fileout])
+    txtout = gr.Textbox(interactive=False)
+    fileout = gr.File(interactive=False)
+    synth.click(gen, inputs=[piano_only], outputs=[txtout, fileout])
 demo.queue().launch()
