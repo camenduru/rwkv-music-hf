8dacb10861dc55cc28cdef2d4def8b8c5da7f29f
mrfakename <mrfakename@users.noreply.huggingface.co>
2024-02-20 21:39:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from musiclib import musicgen
 from io import BytesIO
 import midi_util
 from midi_util import VocabConfig
+import tempfile
 def gen(piano_only, length):
     midi = ''
     for item in musicgen(piano_only=piano_only, length=length):
@@ -12,8 +13,9 @@ def gen(piano_only, length):
     cfg = VocabConfig.from_json('./vocab_config.json')
     text = midi.strip()
     mid = midi_util.convert_str_to_midi(cfg, text)
-    mid.save(file=bio)
-    yield midi, bio.getvalue()
+    with tempfile.NamedTemporaryFile(suffix='.midi', delete=False):
+        mid.save(file=bio)
+        yield midi, bio.getvalue()
 with gr.Blocks() as demo:
     piano_only = gr.Checkbox(label="Piano Only")
     length = gr.Slider(label="Length (in tokens)", minimum=4, maximum=4096, step=1, value=4096)
