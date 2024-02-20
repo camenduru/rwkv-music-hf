016958e4bcfc86adef263db5f83dc228dd2e9a1f
mrfakename <mrfakename@users.noreply.huggingface.co>
2024-02-20 21:19:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,27 +3,22 @@ from musiclib import musicgen
 from io import BytesIO
 import midi_util
 from midi_util import VocabConfig
-from midi2audio import FluidSynth
-fs = FluidSynth()
 def gen(piano_only, length):
     midi = ''
     for item in musicgen(piano_only=piano_only, length=length):
         midi = item
-        yield item, None, None
+        yield item, None
     bio = BytesIO()
     cfg = VocabConfig.from_json('./vocab_config.json')
     text = midi.strip()
     mid = midi_util.convert_str_to_midi(cfg, text)
     mid.save(file=bio)
-    audio = BytesIO()
-    fs.midi_to_audio(bio.getvalue(), audio)
-    return midi, bio.getvalue(), audio.getvalue()
+    return midi, bio.getvalue()
 with gr.Blocks() as demo:
     piano_only = gr.Checkbox(label="Piano Only")
     length = gr.Slider(label="Length (in tokens)", minimum=4, maximum=4096, step=1, value=4096)
     synth = gr.Button("Synthesize")
     txtout = gr.Textbox(interactive=False, label="MIDI Length")
     fileout = gr.File(interactive=False, label="MIDI File")
-    audioout = gr.Audio(interactive=False, label="Audio")
-    synth.click(gen, inputs=[piano_only, length], outputs=[txtout, fileout, audioout])
+    synth.click(gen, inputs=[piano_only, length], outputs=[txtout, fileout])
 demo.queue().launch()
