a66ecf506fa46d1763d6e0be9f5b2b8471f7b10f
mrfakename <mrfakename@users.noreply.huggingface.co>
2024-02-20 02:22:58 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import gradio as gr
+from musiclib import musicgen
+def gen(piano_only):
+    return musicgen(piano_only)
+with gr.Blocks() as demo:
+    piano_only = gr.Checkbox(label="Piano Only")
+    synth = gr.Button("Synthesize")
+    fileout = gr.File(interactive=False)
+    synth.click(gen, inputs=[piano_only], outputs=[fileout])
+demo.queue().launch()
