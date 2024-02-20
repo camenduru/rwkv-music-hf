189a6bf5d321a738dad957a0be192a47b6acc8c3
mrfakename <mrfakename@users.noreply.huggingface.co>
2024-02-20 23:43:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def gen(piano_only, length):
 with gr.Blocks() as demo:
     gr.Markdown("# RWKV 4 Music (MIDI)\n\nThis demo uses the RWKV 4 MIDI model available [here](https://huggingface.co/BlinkDL/rwkv-4-music/blob/main/RWKV-4-MIDI-560M-v1-20230717-ctx4096.pth). Details may be found [here](https://huggingface.co/BlinkDL/rwkv-4-music). The music generation code may be found [here](https://github.com/BlinkDL/ChatRWKV/tree/main/music). The MIDI Tokenizer may be found [here](https://github.com/briansemrau/MIDI-LLM-tokenizer).\n\nNot sure how to play MIDI files? I recommend using the open source [VLC Media Player](https://www.videolan.org/vlc/) with can play MIDI files using FluidSynth.")
     piano_only = gr.Checkbox(label="Piano Only")
-    length = gr.Slider(label="Max Length (in tokens)", minimum=4, maximum=4096, step=1, value=4096, info="The audio may still be shorter than this")
+    length = gr.Slider(label="Max Length (in tokens)", minimum=4, maximum=4096, step=1, value=512, info="The audio may still be shorter than this")
     synth = gr.Button("Synthesize")
     txtout = gr.Textbox(interactive=False, label="MIDI Tokens")
     fileout = gr.File(interactive=False, label="MIDI File", type="binary")
