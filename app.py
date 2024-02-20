5b04ce6ccb6a5cab8756d6bdf553c100548be353
mrfakename <mrfakename@users.noreply.huggingface.co>
2024-02-20 21:13:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def gen(piano_only, length):
     return midi, bio.getvalue(), audio.getvalue()
 with gr.Blocks() as demo:
     piano_only = gr.Checkbox(label="Piano Only")
-    length = gr.Slider(label="Length", minimum=4, maximum=4096, step=1)
+    length = gr.Slider(label="Length", minimum=4, maximum=4096, step=1, value=4096)
     synth = gr.Button("Synthesize")
     txtout = gr.Textbox(interactive=False)
     fileout = gr.File(interactive=False)
