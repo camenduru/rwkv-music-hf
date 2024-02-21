a97e9bba6028bc4078ed1d2fde570419ee6eb62c
mrfakename <mrfakename@users.noreply.huggingface.co>
2024-02-21 16:17:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from glob import glob
 import librosa
 from midi2audio import FluidSynth
 fs = FluidSynth()
-
+import spaces
+@spaces.GPU(enable_queue=True)
 def gen(piano_only, piano_seed, length):
     midi = ''
     if piano_seed:
@@ -28,7 +29,7 @@ def gen(piano_only, piano_seed, length):
         fs.midi_to_audio(tmp.name, aud.name)
         yield midi, tmp.name, aud.name
 with gr.Blocks() as demo:
-    gr.Markdown("# RWKV 4 Music (MIDI)\n\nThis demo uses the RWKV 4 MIDI model available [here](https://huggingface.co/BlinkDL/rwkv-4-music/blob/main/RWKV-4-MIDI-560M-v1-20230717-ctx4096.pth). Details may be found [here](https://huggingface.co/BlinkDL/rwkv-4-music). The music generation code may be found [here](https://github.com/BlinkDL/ChatRWKV/tree/main/music). The MIDI Tokenizer may be found [here](https://github.com/briansemrau/MIDI-LLM-tokenizer).\n\nNot sure how to play MIDI files? I recommend using the open source [VLC Media Player](https://www.videolan.org/vlc/) with can play MIDI files using FluidSynth.\n\nYou are responsible for your usage.\n\n**NOTE:** This Space will be quite slow since it's CPU-only. Please duplicate with a GPU if you'd like a faster version or run it locally")
+    gr.Markdown("# RWKV 4 Music (MIDI)\n\nThis demo uses the RWKV 4 MIDI model available [here](https://huggingface.co/BlinkDL/rwkv-4-music/blob/main/RWKV-4-MIDI-560M-v1-20230717-ctx4096.pth). Details may be found [here](https://huggingface.co/BlinkDL/rwkv-4-music). The music generation code may be found [here](https://github.com/BlinkDL/ChatRWKV/tree/main/music). The MIDI Tokenizer may be found [here](https://github.com/briansemrau/MIDI-LLM-tokenizer).\n\nNot sure how to play MIDI files? I recommend using the open source [VLC Media Player](https://www.videolan.org/vlc/) with can play MIDI files using FluidSynth.\n\nYou are responsible for your usage.")
     piano_only = gr.Checkbox(label="Piano Only")
     piano_seed = gr.Checkbox(label="Use Piano Melody Seed")
     length = gr.Slider(label="Max Length (in tokens)", minimum=4, maximum=4096, step=1, value=512, info="The audio may still be shorter than this")
